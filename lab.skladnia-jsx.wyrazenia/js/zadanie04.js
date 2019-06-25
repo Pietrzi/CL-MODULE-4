5f14110b7215cf1db4d1ae7655c70a71c5c7592b
Pietrzi <pietrzi@op.pl>
2019-06-25 20:50:01 +0200

    zad4 poprawka jsx

--- lab.skladnia-jsx.wyrazenia/js/zadanie04.js
+++ lab.skladnia-jsx.wyrazenia/js/zadanie04.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import Animals from "./data/animals";
 
 ReactDOM.render(
-    <section>{ `Mamy ${Animals.length} zwierząt, a oto one: ${Animals.join(', ')}` }</section>,
+    <section>Mamy {Animals.length} zwierząt, a oto one: {Animals.join(', ')}</section>,
     document.getElementById('app')
 );
 
