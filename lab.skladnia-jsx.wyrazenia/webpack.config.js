2b9b758e6700d338467c3ba68940c0c777669234
Pietrzi <pietrzi@op.pl>
2019-07-02 11:55:35 +0200

    wszystkie zadania

--- lab.skladnia-jsx.wyrazenia/webpack.config.js
+++ lab.skladnia-jsx.wyrazenia/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./js/zadanie04.js",
+  entry: "./js/zadanie01.js",
   output: {
     filename: "out.js",
     path: path.resolve(__dirname, "build")
